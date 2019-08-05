60358e9eb6ca5c8ffdc0093d08f68b6f29458f89
yanghh <136158344@qq.com>
2019-08-05 15:19:10 +0800

    update

---
+++ src/main/java/com/navinfo/autoai/autoailauncher/AutoaiLauncherApplication.java
@@ -0,0 +1,13 @@
+package com.navinfo.autoai.autoailauncher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoaiLauncherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoaiLauncherApplication.class, args);
+    }
+
+}
